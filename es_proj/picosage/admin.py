bb2349719058e7c0bd78db890fbe2220666402b0
Roan Martin-Hayden <roanmh@gmail.com>
2020-02-29 15:24:30 -0500

    Add Admin Interface

--- es_proj/picosage/admin.py
+++ es_proj/picosage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import USAddress, Consumer, Property, Installer, Quote
+
 # Register your models here.
+admin.site.register(USAddress)
+admin.site.register(Consumer)
+admin.site.register(Property)
+admin.site.register(Installer)
+admin.site.register(Quote)
